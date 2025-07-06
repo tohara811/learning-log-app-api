eaa232fb68619c2b64b6d33ecd6201360171c38e
大原朋也 <tohara@kiyo-learning.com>
2025-07-07 06:00:47 +0900

    Fix CORS対策

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors';
 import logs from './logs'
 import register from './users/register'
 import login from './users/login'
 import me from './me'
 
 const app = new Hono()
+
+app.use(
+  '*',
+  cors({
+    origin: 'https://learning-log-app.pages.dev/',
+    credentials: true,
+  })
+);
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
